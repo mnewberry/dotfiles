dcaf4db90c8f6954128cf3678533a29faf4001ee
Mitchell Johnson <mitchell@esau>
2012-06-05 13:34:03 -0400

    notion configuration

---
+++ notion/README.md
@@ -0,0 +1,10 @@
+notion keybindings and austere look
+===================================
+
+The idea behind the key bindings is that movement between frames is with alt +
+vim keys \[hjl\] (k is still the ion metakey).  Movement between objects within
+a frame is alt + shift + \[hl\].  Alt + shift + tab cycles workspaces.
+Mnemonic: The movement keys with shift change what is visible, the movement
+keys without shift just change focus.  Alt + tab by itself used to switch
+between monitors in a multi-screen X configuration, before the xrandr days.
+The rest of the bindings should be similar to ion2.
---
+++ notion/look_tchell.lua
@@ -0,0 +1,165 @@
+-- look_clean.lua drawing engine configuration file for Notion.
+
+if not gr.select_engine("de") then return end
+
+de.reset()
+
+de.defstyle("*", {
+    shadow_colour = "grey",
+    highlight_colour = "grey",
+    background_colour = "#fbfbf1",
+    foreground_colour = "#00454f",
+    padding_pixels = 1,
+    highlight_pixels = 0,
+    shadow_pixels = 0,
+    border_style = "elevated",
+    font = "-*-*-*-*-*-*-6-*-*-*-*-*-*-*",
+    text_align = "center",
+})
+
+de.defstyle("tab", {
+    font = "-*-*-*-*-*-*-6-*-*-*-*-*-*-*",
+    de.substyle("active-selected", {
+        shadow_colour = "white",
+        highlight_colour = "white",
+        background_colour = "#dedddd",
+        foreground_colour = "white",
+    }),
+    de.substyle("active-unselected", {
+        shadow_colour = "grey",
+        highlight_colour = "grey",
+        background_colour = "#fbfbf1",
+        foreground_colour = "#00454f",
+    }),
+    de.substyle("inactive-selected", {
+        shadow_colour = "grey",
+        highlight_colour = "grey",
+        background_colour = "#fbfbf1",
+        foreground_colour = "#00454f",
+    }),
+    de.substyle("inactive-unselected", {
+        shadow_colour = "grey",
+        highlight_colour = "grey",
+        background_colour = "#fbfbf1",
+        foreground_colour = "#00454f",
+    }),
+    text_align = "center",
+})
+
+de.defstyle("input", {
+    foreground_colour = "black",
+    de.substyle("*-cursor", {
+        background_colour = "white",
+        foreground_colour = "#fbfbf1",
+    }),
+    de.substyle("*-selection", {
+        background_colour = "#aaaaaa",
+        foreground_colour = "black",
+    }),
+    font = "-misc-fixed-medium-r-*-*-18-*-*-*-*-*-*-*",
+})
+
+de.defstyle("frame", {
+    background_colour = "#f4f4ed",
+    de.substyle("quasiactive", {
+        -- Something detached from the frame is active
+        padding_colour = "#fbfbf1",
+    }),
+    de.substyle("userattr1", {
+        -- For user scripts
+        padding_colour = "#009010",
+    }),
+    padding_pixels = 0,
+})
+
+de.defstyle("frame-tiled", {
+    shadow_pixels = 0,
+    highlight_pixels = 0,
+    spacing = 0,
+})
+
+--de.defstyle("frame-tiled-alt", {
+--    bar = "none",
+--})
+
+de.defstyle("frame-floating", {
+    --bar = "shaped",
+    padding_pixels = 0,
+})
+
+de.defstyle("frame-transient", {
+    --bar = "none",
+    padding_pixels = 0,
+})
+
+
+de.defstyle("actnotify", {
+    shadow_colour = "#c04040",
+    highlight_colour = "#c04040",
+    background_colour = "#901010",
+    foreground_colour = "#eeeeee",
+})
+
+de.defstyle("tab", {
+    de.substyle("*-*-*-unselected-activity", {
+        shadow_colour = "#c04040",
+        highlight_colour = "#c04040",
+        background_colour = "#901010",
+        foreground_colour = "#eeeeee",
+    }),
+    
+    de.substyle("*-*-*-selected-activity", {
+        shadow_colour = "#c04040",
+        highlight_colour = "#c04040",
+        background_colour = "#b03030",
+        foreground_colour = "#ffffff",
+    }),
+    
+    de.substyle("*-*-*-tabnumber", {
+        background_colour = "#fbfbf1",
+        foreground_colour = "green",
+    }),
+})
+
+de.defstyle("tab-frame", {
+    spacing = 1,
+})
+
+de.defstyle("tab-frame-floating", {
+    spacing = 0,
+})
+
+de.defstyle("tab-menuentry", {
+    text_align = "left",
+})
+
+de.defstyle("tab-menuentry-big", {
+    font = "-*-helvetica-medium-r-normal-*-17-*-*-*-*-*-*-*",
+    padding_pixels = 7,
+})
+
+
+de.defstyle("stdisp", {
+    shadow_pixels = 0,
+    highlight_pixels = 0,
+    text_align = "left",
+    background_colour = "#fbfbf1",
+    foreground_colour = "#00454f",
+    font="-misc-fixed-medium-r-*-*-13-*-*-*-*-60-*-*",
+    
+    de.substyle("important", {
+        foreground_colour = "green",
+    }),
+
+    de.substyle("critical", {
+        foreground_colour = "red",
+    }),
+})
+
+de.defstyle("tab-menuentry-big", {
+    padding_pixels = 7,
+    font = "-misc-fixed-medium-r-*-*-18-*-*-*-*-*-*-*",
+})
+
+gr.refresh()
+
